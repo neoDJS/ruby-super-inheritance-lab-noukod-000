196dba480b8ad033704ce464d64c177bf35cbf7b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-23 20:01:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,10 +4,10 @@ class Student
   end
 
   def raise_hand
-    "Pick me!\n"
+    puts "Pick me!\n"
   end
 
   def hello
-    "Hey there! I'm so excited to learn stuff.\n"
+    puts "Hey there! I'm so excited to learn stuff.\n"
   end
 end
